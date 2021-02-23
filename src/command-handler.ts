a47a7e4b772898591548d6459a0c349bbe0cfba8
cheng <ummcheng@gmail.com>
2021-02-22 18:44:44 -0800

    send source as part of editorState command response

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -1,4 +1,5 @@
 import IPC from "./shared/ipc";
+import { XtermController } from "./xterm-controller";
 
 /*
  * The CommandHandler class is a wrapper around other handlers
@@ -12,8 +13,26 @@ export class CommandHandler {
   // We can use the extension's IPC to send messages back to the client if needed
   ipc?: IPC;
 
+  constructor(private xtermController: XtermController) {}
+
   setIPC(ipc: IPC) {
     this.ipc = ipc;
+  }
+
+  async COMMAND_TYPE_GET_EDITOR_STATE(_data: any): Promise<any> {
+    const source = this.xtermController.command();
+    const cursor = 0;
+
+    return Promise.resolve({
+      message: "editorState",
+      data: {
+        source,
+        cursor,
+        filename: "hyper.sh",
+        files: [],
+        roots: [],
+      },
+    });
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import CommandHandler from "./command-handler";
 import IPC from "./shared/ipc";
 import { XtermController } from "./xterm-controller";
 
-let xtermController: XtermController;
+let xtermController = new XtermController();
 
 export const onRendererWindow = (_window: any) => {
   let commandHandler: CommandHandler;
   let ipc: IPC;
 
   function resetAndConnect() {
-    commandHandler = new CommandHandler();
+    commandHandler = new CommandHandler(xtermController);
     ipc = new IPC(commandHandler, "hyper");
     commandHandler.setIPC(ipc);
 
@@ -21,14 +21,14 @@ export const onRendererWindow = (_window: any) => {
   //   console.log("editorState selection:", xtermController.selection());
   // }, 2000);
 
-  // resetAndConnect();
+  resetAndConnect();
 };
 
 export const getTermProps = (_uid: any, _parentProps: any, props: any) => {
-  // Create a new XtermController if the Terminal instance is new
-  if (props.term && (!xtermController || props.term != xtermController.term)) {
-    console.log("Adding handler")
-    xtermController = new XtermController(props.term);
+  // Update XtermController if the Terminal instance is new
+  if (props.term && props.term != xtermController.term) {
+    console.log("Updating Terminal in xtermController");
+    xtermController.updateTerm(props.term);
   }
   return props;
-}
+};
