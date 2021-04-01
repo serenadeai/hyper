3628e63622004b9e661a72c3b3655714acaa988e
Tommy MacWilliam <tommy@serenade.ai>
2021-04-01 12:31:19 -0700

    use hyper callbacks to retrieve state
    rather than using shell integrations to get the current terminal state,
    use hyper callbacks like onRender, onKey, and onLineFeed. delegate undo
    stack and text entry to the Serenade app rather than handling it here.

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
 import CommandHandler from "./command-handler";
 import IPC from "./shared/ipc";
-import { XtermController } from "./xterm-controller";
+import XtermController from "./xterm-controller";
 
-let xtermController = new XtermController();
+let ipc: IPC;
+let commandHandler: CommandHandler;
+let xtermController: XtermController = new XtermController();
 
 export const onRendererWindow = (_window: any) => {
-  let commandHandler: CommandHandler;
-  let ipc: IPC;
-
-  function resetAndConnect() {
+  const resetAndConnect = () => {
     commandHandler = new CommandHandler(xtermController);
     ipc = new IPC(commandHandler, "hyper");
-    commandHandler.setIPC(ipc);
-
     ipc.start();
-  }
+  };
 
   resetAndConnect();
 };
 
 export const getTermProps = (_uid: any, _parentProps: any, props: any) => {
-  // Update XtermController if the Terminal instance is new
-  if (props.term && props.term != xtermController.term) {
-    console.log("Updating Terminal in xtermController");
-    xtermController.updateTerm(props.term);
-  }
+  xtermController.setTerminal(props.term);
   return props;
 };
--- src/xterm-controller.ts
+++ src/xterm-controller.ts
@@ -1,159 +1,81 @@
-import { Terminal } from "xterm";
-import { cursorMove } from "ansi-escapes";
+interface Prompt {
+  prefix: string;
+  offsetLeft: number;
+  bufferIndex: number;
+}
 
-const debug = false;
-const log = (...args: any[]) => {
-  if (debug) {
-    console.log(...args);
-  }
-};
+export default class XtermController {
+  private prompt: Prompt = { prefix: "", offsetLeft: 0, bufferIndex: 0 };
+  private terminal: any;
+  private updateOnRender: boolean = false;
 
-/*
-    Controls Xterm.js's representation of the underlying terminal using
-    escape codes and buffer diffs.
- */
-
-export class XtermController {
-  private preCommandBuffer: string[] = [];
-  private command: string = "";
-  private cursor: number = 0;
-
-  private clearedState = true;
-
-  constructor(public term?: Terminal) {
-    if (term) {
-      this.updateTerm(term);
+  private getActiveLine(): string {
+    const buffer = this.terminal.buffer.active;
+    let i = this.getActiveLineNumber();
+    let line = buffer.getLine(i).translateToString().trimRight();
+    while (i > 0 && buffer.getLine(i).isWrapped) {
+      line = buffer.getLine(i - 1).translateToString() + line;
+      i--;
     }
+
+    return line;
   }
 
-  updateTerm(term: Terminal) {
-    this.term = term;
-    // Register handlers.
-    this.registerHandlers(term);
-    term.onRender(() => {
-      log("onRender", "clearedState", this.clearedState);
-
-      if (!this.clearedState) {
-        this.savePreviousState();
-        this.clearedState = true;
-        log("state", this.state());
+  private getActiveLineNumber(): number {
+    const buffer = this.terminal.buffer.active;
+    for (let i = buffer.length - 1; i >= 0; i--) {
+      if (
+        buffer.getLine(i).isWrapped ||
+        !/^\s*$/.test(buffer.getLine(i).translateToString().trimRight())
+      ) {
+        return i;
       }
-    });
+    }
+
+    return 0;
   }
 
-  private registerHandlers(term: Terminal) {
-    term.parser.registerOscHandler(133, (data) => {
-      switch (data[0]) {
-        case "A":
-          log("Prompt started");
-          break;
-        case "B":
-          log("Command start");
-          this.clearedState = false;
-          // this.savePreviousState();
-          log("state", this.state());
-          break;
-        case "C":
-          log("Command executed");
-          log("state", this.state());
-          this.savePreviousState();
-          break;
-        case "D":
-          log("Command finished");
-          this.savePreviousState();
-          log("state", this.state());
-          break;
-        default:
-          console.warn("Unknown code", data);
-      }
-
-      return true;
-    });
-  }
-
-  state = () => {
-    this.updateState();
-    return {
-      source: this.command,
-      cursor: this.cursor,
+  private updatePrompt() {
+    this.prompt = {
+      prefix: this.getActiveLine(),
+      offsetLeft: this.terminal.buffer.active.cursorX,
+      bufferIndex: this.getActiveLineNumber(),
     };
-  };
+  }
 
-  setCursor = (cursor: number) => {
-    this.updateState();
-    log("adjusting cursor by", cursor - this.cursor);
-    this.term?.write(cursorMove(cursor - this.cursor));
-  };
-
-  erase = (count: number) => {
-    if (count) {
-      this.term?.write("\u001B[" + count + "\b");
-    }
-  };
-
-  write = (diff: string) => {
-    this.term?.write(diff);
-  };
-
-  private updateState = () => {
-    if (this.term === undefined) {
+  setTerminal(terminal: any) {
+    if (!terminal || this.terminal == terminal) {
       return;
     }
 
-    let command = "";
-    let cursor = 0;
+    this.terminal = terminal;
 
-    const count = (substring: string, y: number, offset: number = 0) => {
-      // Ignore empty lines
-      if (substring === "") {
-        return;
+    this.terminal.onRender((data: any) => {
+      if ((data.start == 0 && data.end == this.terminal.rows - 1) || this.updateOnRender) {
+        this.updatePrompt();
       }
+    });
 
-      command += substring + "\n";
-      // If we haven't reached the row of the cursor, add the length of the substring
-      if (y < this.term!.buffer.cursorY) {
-        cursor += substring.length + 1;
-      }
+    this.terminal.onKey((_key: any) => {
+      this.updateOnRender = false;
+    });
 
-      // If we're on the row of the cursor, add up to the column of the cursor, minus the offset
-      if (y === this.term!.buffer.cursorY) {
-        cursor += this.term!.buffer.cursorX - offset;
-      }
+    this.terminal.onLineFeed(() => {
+      this.updateOnRender = true;
+    });
+
+    setTimeout(() => {
+      this.updatePrompt();
+    }, 200);
+  }
+
+  state(): { source: string; cursor: number } {
+    return {
+      source: this.getActiveLine().substring(this.prompt.offsetLeft),
+      cursor:
+        this.terminal.cols * (this.getActiveLineNumber() - this.prompt.bufferIndex) +
+        this.terminal.buffer.active.cursorX -
+        this.prompt.offsetLeft,
     };
-
-    // Look at every row in the buffer
-    for (let i = 0; i < this.term!.buffer.length; i++) {
-      const currentLine = this.term!.buffer.getLine(i)?.translateToString().trimRight() || "";
-      // If this line is the same as before, skip
-      if (currentLine === this.preCommandBuffer[i]) {
-        continue;
-      }
-
-      let substring = currentLine;
-      let offset = 0;
-
-      // Compare to the previous buffer to get a substring and offset
-      if (this.preCommandBuffer[i]) {
-        for (let j = 0; j < this.preCommandBuffer[i].length; j++) {
-          if (currentLine[j] !== this.preCommandBuffer[i][j]) {
-            substring = currentLine.substring(j);
-            offset = j;
-            break;
-          }
-        }
-      }
-
-      count(substring, i, offset);
-    }
-
-    this.command = command;
-    this.cursor = cursor;
-  };
-
-  private savePreviousState = () => {
-    this.preCommandBuffer = [];
-    for (let i = 0; i < this.term!.buffer.length; i++) {
-      this.preCommandBuffer.push(this.term!.buffer.getLine(i)?.translateToString() || "");
-    }
-  };
+  }
 }
